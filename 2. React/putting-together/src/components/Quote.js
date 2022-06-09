2c9cd46727ddd2d82e679998cd30a99b66cb2488
xtina-lt <xtina_lt@outlook.com>
2022-06-09 11:27:25 -0400

    change quotes per click, take out body replace with empty arrows

--- 2. React/putting-together/src/components/Quote.js
+++ 2. React/putting-together/src/components/Quote.js
@@ -1,29 +1,49 @@
 import React from 'react';
 
-const Quote = (props) => {
+// GET A NEW QUOTE FOR EACH BUTTON CLICK / HIDE
+const change = () => {   
+    // DEFINE QUOTES
+    let t = "Quote:";
+    let arr = [
+            {title : t,
+            desc:"Life is a promise.. Fulfill it",
+            img:"https://m.lovethispic.com/uploaded_images/blogs/Inspiring-Gif-Quotes-Of-The-Day-49125-2.gif"
+            },
+        {
+            title : t,
+            desc : "Find a Reason to Smile",
+            img: "https://i.gifer.com/KuhU.gif"
+        },
+        {
+            title : t,
+            desc : "Be Addicted to Your Passion",
+            img : "https://c.tenor.com/V62KtXdTJQQAAAAC/motivational-quotes.gif"
+        }
+    ]
+    // get random indexfrom arr
+    let rand = Math.floor(Math.random() * arr.length);
+
+    // CHANGE HTML
+    // get id
+    let x = document.getElementById("change-me");
+    // change innerHtml
+    x.innerHTML =`<h1> ${arr[rand].title} </h1> <h2> "${arr[rand].desc}!" </h2> <img src=${arr[rand].img} alt="quote" />`;
+    // switch display
+    (x.style.display === 'inline') ? x.style.display = 'none' : x.style.display = 'inline';
+}
+
+const Quote = () => {
     return (
         <div>
-            {/* <h2>
-                XTINA.CODES
-            </h2>
-            <ul>
-                <li>
-                    Coding experience: 1 year.
-                </li>
-                <li>
-                    Currently: conquering world.
-                </li>
-                <li>
-                    <img src="https://i.pinimg.com/564x/fb/43/d0/fb43d01060509d93eda18e4b3b65c1e8.jpg" alt="inspirational"></img>
-                </li>
-            </ul> */}
-            <h1>
-                { props.title }
-            </h1>
-            <h2>
-                "{ props.desc }!"
-            </h2>
-            <img src={ props.img } alt="quote"/>
+            <span id="change-me" style={{
+                display: 'none',
+            }}>
+
+            </span>
+
+            <button onClick={() => change()}>
+                Click Me
+            </button>
         </div>
     );
 }
