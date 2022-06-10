4f00c663762e5c4b8a75f18918b5a73922a42bd3
xtina-lt <xtina_lt@outlook.com>
2022-06-10 10:55:10 -0400

    more forms/validation

--- 2. React/form-play/src/components/UserForm.js
+++ 2. React/form-play/src/components/UserForm.js
@@ -1,57 +1,90 @@
 import React, {useState} from 'react';
+// uppercase and lowercase
+// digits 0-9
+// characters ! # $ % & ' * + - / = ? ^_ ` { | } ~
+// charcter ".", not first or last, doesnt occur twice
+// eslint-disable-next-line
+const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const reP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
 const UserForm = (props) =>{
-    const [uName, setUName] = useState("");
+    const [first, setFirst] = useState("");
+    const [last, setLast] = useState("");
     const [email, setEmail] = useState("");
     const [confirmE, setConfirmE] = useState("");
     const [password, setPassword] = useState("");
     const [confirmP, setConfirmP] = useState("");
+    const [isSubmit, setSubmit] = useState(false);
 
     const createUser = (e) => {
         e.preventDefault();
 
-        const newUser = {uName, email, confirmE, password, confirmP};
+        const newUser = {first, last, email, confirmE, password, confirmP};
         console.log("Welcome, ", newUser);
-        setUName("");
+        setFirst("");
+        setLast("");
         setEmail("");
         setConfirmE("");
         setPassword("");
         setConfirmP("");
+        setSubmit(true);
     }
+
+    const getSumbit = () => {return (isSubmit) ? "Thanks for sumbitting!" : "Please submit...";}
 
     return(
         <div>
+            <h3>
+                {getSumbit()}
+            </h3>
             <form onSubmit = { createUser }>
+                {/* first name */}
                 <label>
-                    Username:
+                    First Name:
                 </label>
-                <input type="text" value={uName} onChange={(e)=> setUName(e.target.value)}/>
+                {(first.length < 2 && first.length > 0)? <p>Try 🥈 characters.</p>: null}
+                <input type="text" value={first} onChange={(e)=> setFirst(e.target.value)}/>
+                {/* last name */}
+                <label>
+                    Last Name:
+                </label>
+                {(last.length < 2 && last.length > 0)? <p>Try 🥈 characters.</p>: null}
+                <input type="text" value={last} onChange={(e)=> setLast(e.target.value)}/>
+                {/* email */}
                 <label>
                     Email
                 </label>
+                {(!email.match(re) && email.length > 0) ? <p>Try a VALID email 👼.</p> : null}
                 <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                {/* confirm email */}
                 <label>
                     Confirm Email
                 </label>
-                <input type="text" value={confirmE} onChange={(e)=> setConfirmP(e.target.value)}/>
+                {(confirmE !== email && confirmE.length > 0) ? <p>Let's play the matching game</p> : null}
+                <input type="text" value={confirmE} onChange={(e)=> setConfirmE(e.target.value)}/>
+                {/* password */}
                 <label>
                     Password:
                 </label>
+                {(!password.match(reP) && password.length > 0) ? <p>Must be: 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character.</p> : null}
                 <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                {/* check password */}
                 <label>
                     Confirm Password:
                 </label>
-                <input type="confirmP" value={confirmP} onChange={(e)=> setConfirmP(e.target.value)}/>
+                {(confirmP !== password && confirmP.length > 0) ? <p>Let's play the matching game</p> : null}
+                <input type="password" value={confirmP} onChange={(e)=> setConfirmP(e.target.value)}/>
+                {/* submit */}
                 <input type ="submit" value="Register"/>
             </form>
 
             <table>
                 <tr>
                     <td>
-                        User Name:
+                        Name:
                     </td>
                     <td>
-                        {uName}
+                        {last}, {first}
                     </td>
                 </tr>
                 <tr>
