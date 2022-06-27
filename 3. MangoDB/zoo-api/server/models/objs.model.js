4600575391204a95335a40d49928e5bbc5986cab
xtina-lt <xtina_lt@outlook.com>
2022-06-27 13:22:01 -0400

    larger validation comment

--- 3. MangoDB/zoo-api/server/models/objs.model.js
+++ 3. MangoDB/zoo-api/server/models/objs.model.js
@@ -9,6 +9,25 @@ const Schema = new mongoose.Schema(
         interests : []
     }
 );
+/* const Schema = new mongoose.Schema(
+    {
+        _id : {
+            type: Number,
+            required: [true, "id required"],
+            min[1, 'Must be larger than 1'],
+            max[1084723, 'Must be smaller than 1084723']
+        }
+        name : {
+            type: String,
+            required: [true, "entry required"],
+            minlength[2, 'Must be larger than 2'],
+            max[20, 'Must be smaller than 20']
+        }
+        
+    }
+)
+*
+*/
 
 console.log("Schema" + Schema);
 
